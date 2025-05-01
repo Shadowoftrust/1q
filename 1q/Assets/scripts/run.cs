5c16e311fe89f80711fffb022c54d97d07bfb738
Carlos Castellanos <karlos065@hotmail.com>
2025-05-01 15:52:36 -0500

    Prueba hola

--- 1q/Assets/scripts/run.cs
+++ 1q/Assets/scripts/run.cs
@@ -9,8 +9,6 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
     public float jumpForce = 9f;
 
-    //hola
-
     [Header("Chequeo de suelo")]
     public Transform groundCheck;
     public float groundCheckRadius = 0.4f;
